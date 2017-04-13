6e8d2836bb28e83555722292ef63fe065718f1b3
Dave Brotherstone <davegb@pobox.com>
2017-04-13 22:39:26 +0200

    Fix permissions on private key

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+chmod 600 deploy/id_rsa_deploy
+
 SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -B -i deploy/id_rsa_deploy -P $SSHPORT"
 # Transfer the zip first so we don't break anyone downloading the new md5 and old zip
 scp $SSH_OPTIONS plugins64.zip xmldata@nppxmldev.bruderste.in:/data/dev/content/xml
