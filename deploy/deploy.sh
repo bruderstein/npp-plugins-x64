907a07be23140691f297900ef7d4c8cec8408c9d
Dave Brotherstone <davegb@pobox.com>
2017-04-13 22:34:44 +0200

    Remove quiet mode to see the error
    Getting CI right always ends up a series of "this time", "argh try
    again" commit messages...

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -B -q -i deploy/id_rsa_deploy -P $SSHPORT"
+SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -B -i deploy/id_rsa_deploy -P $SSHPORT"
 # Transfer the zip first so we don't break anyone downloading the new md5 and old zip
 scp $SSH_OPTIONS plugins64.zip xmldata@nppxmldev.bruderste.in:/data/dev/content/xml
 
