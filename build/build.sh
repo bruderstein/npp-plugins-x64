532d5e131e4b5af1e533dd1694c0ce08b9cf6e51
Dave Brotherstone <davegb@pobox.com>
2017-04-14 14:50:53 +0200

    Add validate.json support

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,7 @@
 xmllint --noblanks plugins/plugins64.xml > PluginManagerPlugins.xml
 zip plugins64.zip PluginManagerPlugins.xml
 md5sum PluginManagerPlugins.xml | sed -E -e 's/^([0-9a-f]+)\s.*$/\1/' | tr -d '\n' > plugins64.md5.txt
+jq '. | map(.) | add | keys | reduce .[] as $item ({}; .[$item] = "ok")' plugins/validate.json > validate64.json
 
 
 
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,11 @@ set -e
 chmod 600 deploy/id_rsa_deploy
 
 SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -B -i deploy/id_rsa_deploy -P $SSHPORT"
-# Transfer the zip first so we don't break anyone downloading the new md5 and old zip
+
+# Transfer the validate first so anyone who get's the new zip will also get the new validation
+scp $SSH_OPTIONS validate64.json xmldata@nppxmldev.bruderste.in:/data/dev/content/validate
+
+# Transfer the zip next so we don't break anyone downloading the new md5 and old zip
 scp $SSH_OPTIONS plugins64.zip xmldata@nppxmldev.bruderste.in:/data/dev/content/xml
 
 scp $SSH_OPTIONS plugins64.md5.txt xmldata@nppxmldev.bruderste.in:/data/dev/content/xml
