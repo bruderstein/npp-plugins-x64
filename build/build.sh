a88ee49e02ce8b4d87a8d227552995350ed66079
Christian Grasser <christian.grasser@live.de>
2017-09-14 20:23:16 +0200

    see https://github.com/bruderstein/npp-plugins-x64/issues/33, try to check program existence before execution and fail without uploading the empty/broken list

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+if ! [ -x "$(command -v xmllint)" ]; then
+  echo 'Error: xmllint is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v zip)" ]; then
+  echo 'Error: zip is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v md5sum)" ]; then
+  echo 'Error: md5sum is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v jq)" ]; then
+  echo 'Error: jq is not installed.' >&2
+  exit 1
+fi
+
 xmllint --noblanks plugins/plugins64.xml > PluginManagerPlugins.xml
 zip plugins64.zip PluginManagerPlugins.xml
 md5sum PluginManagerPlugins.xml | sed -E -e 's/^([0-9a-f]+)\s.*$/\1/' | tr -d '\n' > plugins64.md5.txt
